b72f4d261d8abfec7eaa72dd3c4a8ca3fb7ef941
Peter Kim <peter.kim@uconn.edu>
2022-10-10 01:02:11 -0400

    latitude and longitude

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,8 @@
 // parse data for lat + lon
 // input lat + lon into the api to get the weather forecast
 // populate the containers with the parsed weather info
-
+var cityLatitude;
+var cityLongitude;
 
 
 var formSubmitHandler = function (event) {
@@ -10,8 +11,7 @@ var formSubmitHandler = function (event) {
     var cityName = document.querySelector("#cityName").value.trim();
     if (cityName) {
         var geoApiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=308207fba2f306021f2d0ad086ed7b2f"
-        alert(cityName);
-        console.log("hello");
+        getCityData(geoApiUrl);
         document.querySelector("#cityName").value = "";
     } 
     else {
@@ -19,5 +19,23 @@ var formSubmitHandler = function (event) {
         console.log("hello")
     }
 };
+
+var getCityData = function (url) {
+    fetch(url)
+        .then(function (response) {
+            if(response.ok) {
+                response.json().then(function (data) {
+                    cityLatitude = data[0].lat;
+                    cityLongitude = data[0].lon;
+                    console.log(cityLatitude + ", " + cityLongitude)
+                })
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+    .catch(function (error) {
+        alert("unable to connect to servers")
+    });
+}
 
 document.querySelector("#cityNameSearch").addEventListener('submit', formSubmitHandler);
