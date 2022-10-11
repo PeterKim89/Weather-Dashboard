e3dd7934a97f4bb2c573e12b61b0fcb04982cc55
Peter Kim <peter.kim@uconn.edu>
2022-10-10 23:32:41 -0400

    JS comments added

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
-// input city name
-// parse data for lat + lon
-// input lat + lon into the api to get the weather forecast
-// populate the containers with the parsed weather info
+// object to target Current Day elements
 var currentForecastObj = {
     date: "#currentDate",
     icon: "#currentIcon",
@@ -9,6 +6,7 @@ var currentForecastObj = {
     wind: "#currentWind",
     humidity: "#currentHumidity"
 }
+// object to target future day elements
 var forecastContainersObj = [ 
     {
         date: "#date1",
@@ -47,17 +45,21 @@ var forecastContainersObj = [
     }
 ]
 
+// Will trigger upon a form submit of the city input
 var formSubmitHandler = function (event) {
     event.preventDefault();
+    // will save the city input into a variable
     var cityName = document.querySelector("#cityName").value.trim();
     if (cityName) {
-        console.log(cityName);
+        // creating the city specific api url
         var geoApiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=308207fba2f306021f2d0ad086ed7b2f&units=imperial"
+        // passing the api call along with the city's name to another function to get the actual weather data
         getCityData(geoApiUrl, cityName);
         // passes the city name value to the Current Day Forecast
         document.querySelector("#cityName").value = "";
         // append a button to the search history with the city name
         var pastSearch = document.createElement("button");
+        // set it's id and class to style and target later
         pastSearch.setAttribute("id", cityName+"Btn");
         pastSearch.setAttribute("class", "historyButton");
         pastSearch.innerHTML = cityName;
@@ -65,6 +67,7 @@ var formSubmitHandler = function (event) {
         pastSearch.addEventListener("click", function(){
             getCityData("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=308207fba2f306021f2d0ad086ed7b2f&units=imperial", cityName)
         }); 
+        // append the button onto the search history
         document.querySelector("#searchHistoryContainer").append(pastSearch);
 
     } 
@@ -79,7 +82,7 @@ var getCityData = function (url, city) {
         .then(function (response) {
             if(response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
+                    // assigning data to the current day info
                     document.querySelector("#searchCityName").textContent = city;
                     document.querySelector(currentForecastObj.date).textContent = moment().format("MMMM, Do, YYYY");
                     document.querySelector(currentForecastObj.icon).src = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png"
@@ -87,21 +90,21 @@ var getCityData = function (url, city) {
                     document.querySelector(currentForecastObj.wind).textContent = " " + data.wind.speed + " mph";
                     document.querySelector(currentForecastObj.humidity).textContent = " " + data.main.humidity + "%";
 
+                    // retrieving coordinates from the parsed json data
                     var cityLatitude = data.coord.lat; 
                     var cityLongitude = data.coord.lon;
                     
                     // add city + coordinates to local storage
                     localStorage.setItem(city, [cityLatitude,cityLongitude])
 
+                    // api call for the future 5 day forecast
                     var weatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+cityLatitude+"&lon="+cityLongitude+"&appid=fada60c7d193a5d0339eb9c9f0107eb5&units=imperial";
                     fetch(weatherApiUrl)
                         .then (function (response) {
                             if(response.ok) {
                                 response.json().then(function (data2) {
-                                    console.log(data2);
                                     cityDataObject = data2;
-                                    // console.log(cityDataObject);
-                                    for (i=0; i<5; i++) //TODO: change i<5 back to i<6
+                                    for (i=0; i<5; i++)
                                     {
                                         var iteratedDate = moment();
                                         iteratedDate = iteratedDate.add(i+1,'d');
@@ -109,14 +112,13 @@ var getCityData = function (url, city) {
                                         document.querySelector(forecastContainersObj[i].date).textContent = " " + iteratedDate.format("MMMM, Do, YYYY");
                                         document.querySelector(forecastContainersObj[i].temp).textContent = " " + data2.list[8*i+3].main.temp + "F";
                                         document.querySelector(forecastContainersObj[i].wind).textContent = " " + data2.list[8*i+3].wind.speed + " mph"; 
-                                        document.querySelector(forecastContainersObj[i].humidity).textContent = " " + data2.list[8*i+3].main.humidity + "%";
-                                        
+                                        document.querySelector(forecastContainersObj[i].humidity).textContent = " " + data2.list[8*i+3].main.humidity + "%";        
                                     }
                                 })
                             }
                         })
                         .catch(function (error) {
-                            alert("Error: "+ response.statusText);
+                            alert("Unable to connect to servers");
                         })
                 })
             } else {
@@ -127,7 +129,5 @@ var getCityData = function (url, city) {
         alert("unable to connect to servers");
     });
 }
-
-
 
 document.querySelector("#cityNameSearch").addEventListener("submit", formSubmitHandler);
